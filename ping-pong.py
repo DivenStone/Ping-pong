2ec7841da504f869b1763c19de634e2d1f78abf1
DivenStone <105007440+DivenStone@users.noreply.github.com>
2022-05-25 18:58:23 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -6,6 +6,13 @@ clock = time.Clock()
 FPS = 60
 font.init()
 
+speed_x = 3
+speed_y = 3
+
+font1 = font.Font(None, 35)
+lose = font1.render('Player 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('Player 2 LOSE!', True, (180, 0, 0))
+
 screen = display.set_mode((700, 500))
 display.set_caption("ping-pong")
 
@@ -36,9 +43,11 @@ class Player(GameSprite):
         if keys_t[K_DOWN] and self.rect.y < 615:
             self.rect.y += self.speed
 
+ball = GameSprite('png-transparent-yellow-ball-illustration-table-tennis-ball-ping-pong-ball-sport-orange-ball.png',250,250,5,50,50)
 player = Player('ddd.png', 10, 230, 20, 100, 7)
 player1 = Player('ddd.png', 670, 230, 20, 100, 7)
 
+finish = False
 game = True
 while game:
     screen.blit(background,(0, 0))
@@ -46,9 +55,25 @@ while game:
         
         if e.type == QUIT:
             game = False
-    player.update_r()
-    player.reset()
-    player1.update_l()
-    player1.reset()
-    display.update()
-    clock.tick(FPS)
+    if finish != True:
+        player.update_l()
+        player.reset()
+        ball.reset()
+        player1.update_r()
+        player1.reset()
+        ball.rect.x += speed_x
+        ball.rect.y +=speed_y
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x > 670 or ball.rect.y < 0:
+            speed_x *= -1
+        if sprite.collide_rect(player, ball) or sprite.collide_rect(player1, ball):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            screen.blit(lose, (200, 200))
+        if ball.rect.x > 670:
+            finish = True
+            screen.blit(lose2, (200, 200))
+        display.update()
+        clock.tick(FPS)
